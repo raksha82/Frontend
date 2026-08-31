9156946f32e735e5b725ac88dfe5b37265a9717e
raksha82 <rakshamurugesan82@gmail.com>
2026-08-31 22:21:48 +0530

    nested

---
+++ Js/Conditional Statement/Nested.js
@@ -0,0 +1,24 @@
+// Find the largest Number
+
+let a=90 , b=80 , c=120;
+
+if(a>b)
+{
+    if(a>c)
+    {
+        console.log("A is greater than B and C");
+    }
+}
+
+else
+{
+    if(b>c)
+    {
+
+    }
+
+    else
+    {
+        
+    }
+}
