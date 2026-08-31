462b6910ce6dab09c86b0107deafa4c442646d32
raksha82 <rakshamurugesan82@gmail.com>
2026-08-31 22:34:14 +0530

    switch

--- Js/Conditional Statement/switch.js
+++ Js/Conditional Statement/switch.js
@@ -1,22 +1,33 @@
-// Switch statement
-
-let day = 3;
+let day = 4;
 
 switch (day) {
+
     case 1:
-        console.log("Monday");
+        console.log("Sunday");
         break;
 
     case 2:
-        console.log("Tuesday");
+        console.log("Monday");
         break;
 
     case 3:
-        console.log("Wednesday");
+        console.log("Tuesday");
         break;
 
     case 4:
+        console.log("Wednesday");
+        break;
+
+    case 5:
         console.log("Thursday");
+        break;
+    
+    case 6:
+        console.log("Friday");
+        break;
+
+    case 7:
+        console.log("Saturday");
         break;
 
     default:
