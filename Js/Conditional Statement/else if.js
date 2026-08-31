0bf7083fd7617f604c42c9dde523817144d2d58a
raksha82 <rakshamurugesan82@gmail.com>
2026-08-31 22:17:36 +0530

    else if

--- Js/Conditional Statement/else if.js
+++ Js/Conditional Statement/else if.js
@@ -1,1 +1,41 @@
 let mark=80;
+
+if(mark>90 && mark<=100)
+{
+    console.log("O grade");
+}
+
+else if(mark>80 && mark<=90)
+{
+    console.log("A+ grade");
+}
+
+else if(mark>70 && mark<=80)
+{
+    console.log("A grade");
+}
+
+else if(mark>60 && mark<=70)
+{
+    console.log("B+ grade");
+}
+
+else if(mark>50 && mark<=60)
+{
+    console.log("B grade");
+}
+
+else if(mark>40 && mark<=50)
+{
+    console.log("C grade");
+}
+
+else if(mark>0 && mark<=40)
+{
+    console.log("Fail");
+}
+
+else
+{
+    console.log("Enter the Valid mark");
+}
