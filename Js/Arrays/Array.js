1e926f42f9b8711a576d0ce020ad22e2c7bcc3cf
raksha82 <rakshamurugesan82@gmail.com>
2026-09-03 08:49:24 +0530

    Array Creation

--- Js/Arrays/Array.js
+++ Js/Arrays/Array.js
@@ -2,3 +2,10 @@
 
 let fruits=["Apple","Mango","Pineapple","Guava"];
 console.log(fruits);
+
+
+
+// Creating array
+
+const students=["Raksha" , "Rakshan" , "Ranjan" , "Varun"];
+console.log(students);
