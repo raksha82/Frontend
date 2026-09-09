70b7610a976ed88496d1af88e228215398066d68
raksha82 <rakshamurugesan82@gmail.com>
2026-09-09 19:51:04 +0530

    Callback Function

--- Function/Callback.js
+++ Function/Callback.js
@@ -1,1 +1,14 @@
 // Callback Function
+
+function addition(a,b,callback)
+{
+    console.log(a+b);
+    callback(a,b);
+}
+
+function multiply(x,y)
+{
+    console.log(x*y);
+}
+
+addition(10,20,multiply)
