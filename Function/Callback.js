d68782750411bbff9d17903a131c633f0b2f808b
raksha82 <rakshamurugesan82@gmail.com>
2026-09-09 20:06:43 +0530

    Why callback

--- Function/Callback.js
+++ Function/Callback.js
@@ -1,14 +1,37 @@
-// Callback Function
+// // Callback Function
 
-function addition(a,b,callback)
+// function addition(a,b,callback)
+// {
+//     console.log(a+b);
+//     callback(a,b);
+// }
+
+// function multiply(x,y)
+// {
+//     console.log(x*y);
+// }
+
+// addition(10,20,multiply)
+
+
+
+// Why calllback
+
+function add(a,b,callback)
 {
     console.log(a+b);
-    callback(a,b);
+    callback(a,b,sub);
 }
 
-function multiply(x,y)
+function sub(x,y,callback)
 {
-    console.log(x*y);
+    console.log(x-y);
+    callback(x,y,multiplication)
 }
 
-addition(10,20,multiply)
+function multiplication(a,b)
+{
+    console.log(a*b);
+}
+
+add(50,2,sub);
