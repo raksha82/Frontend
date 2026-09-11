b077a86deb5913f7541ea988cf595b75068614cc
raksha82 <rakshamurugesan82@gmail.com>
2026-09-11 11:21:41 +0530

    Let Datatype

--- Js/Variables/let.js
+++ Js/Variables/let.js
@@ -2,6 +2,25 @@
 let a=40;
 console.log(a); //40
 
-// & Re-Declaration
+// & Re-Initialization
 a=50.78;
 console.log(a); //50.78
+
+// & Re-Declaration
+// let a=67;
+console.log(a); //SyntaxError: Identifier 'a' has already been declared
+
+
+// & Re-Declaration in different block
+function call(){
+    let b=10;
+    console.log(b); //10
+}
+
+let b=40;
+call()
+console.log(b); //40
+
+
+
+
