55266b7a393e4685cd9a6350082cd37101cf9b1d
raksha82 <rakshamurugesan82@gmail.com>
2026-09-11 11:15:11 +0530

    Var Datatype

--- Js/Variables/var.js
+++ Js/Variables/var.js
@@ -1,10 +1,18 @@
 // ! Var
-
 var a=10;
-console.log(a);
+console.log(a);  // 10
 
 // ! Re-Initializing
-
 a=20;
+console.log(a);  // 20
 
-console.log(a);
+// ! Re -declaring
+var a=45.6;
+console.log(a); //45.6
+
+
+
+
+
+
+
