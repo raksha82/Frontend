684fbd939f020101e8c4835d94e5fd121a604bd3
raksha82 <rakshamurugesan82@gmail.com>
2026-09-16 21:07:41 +0530

    Boolean + String

--- Js/Type Conversion/Implicit.js
+++ Js/Type Conversion/Implicit.js
@@ -9,8 +9,12 @@ console.log(name+num);
 // Number + String = String
 console.log(num+name);
 
-// String + Boolean
+// String + Boolean = String
 let animal="Dog";
 let isanimal=true;
 console.log(animal+isanimal);
+
+// Boolean + String = String
+console.log(isanimal+animal);
+
 
