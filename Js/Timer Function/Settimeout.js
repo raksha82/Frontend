ad48fc5ed0b9301a63d3eb9ca04b7a2aca1a596d
raksha82 <rakshamurugesan82@gmail.com>
2026-09-19 21:10:58 +0530

    Settimeout

---
+++ Js/Timer Function/Settimeout.js
@@ -0,0 +1,14 @@
+// setTimeout(()=>
+// {
+//     console.log("Raksha");
+// },2000);
+
+// setTimeout(()=>
+// {
+//     console.log("Rakshan");
+// },1000);
+
+setInterval(()=>{
+    console.log("Ranjan I love you");
+
+},2000)
