4ad73935c56e6fe9d13d2f0e1ba9c2c9d7be9434
raksha82 <rakshamurugesan82@gmail.com>
2026-09-20 16:30:20 +0530

    HTML

---
+++ Js/Events/script.js
@@ -0,0 +1,18 @@
+let button=document.getElementById("click");
+let text=document.getElementById("text");
+
+button.addEventListener("click" ,()=>{
+
+    setTimeout(()=>{
+    text.textContent="Hello , this is events";
+    text.style.color="green";
+    },2000)
+
+});
+
+button.addEventListener("click", function() {
+    console.log("Second function");
+});
+
+
+
